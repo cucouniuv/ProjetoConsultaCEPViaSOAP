7b897019d77a4ad64cd618a2308dc5cf3b826a8c
Luis <luis.olivetti@db1.com.br>
2020-09-07 21:57:26 -0300

    Adicionar

---
+++ README.md
@@ -0,0 +1,20 @@
+# O que é este projeto?
+Projeto de estudo sobre consumir um WebService via SOAP com Web API .NET Core
+
+Conteúdo que utilizei como referência:
+
+https://thiagoborges.net.br/como-consumir-um-web-service-no-c/
+
+# URL do serviço dos Correios
+
+`https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl`
+
+# Para testar
+
+GET para `https://localhost:5001/Correios/88053-454`
+
+# Criado com
+
+* Microsoft Visual Studio Community 2019
+* .NET Core 3.1
+
---
+++ SOAP/Controllers/CorreiosController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SOAP.Models;
+using ServicoCorreios;
+
+namespace SOAP.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CorreiosController : ControllerBase
+    {
+        [HttpGet("{cep}")]
+        public async Task<IActionResult> GetDadosCep(string cep)
+        {
+            try
+            {
+                var cliente = new AtendeClienteClient();
+                var consulta = await cliente.consultaCEPAsync(cep);
+
+                if ((consulta == null) || (consulta.@return == null))
+                    return NotFound("Retorno inexistente ou CEP não encontrado");
+
+                Endereco endereco = new Endereco
+                {
+                    Descricao = consulta.@return.end,
+                    Complemento = consulta.@return.complemento2,
+                    Bairro = consulta.@return.bairro,
+                    Cidade = consulta.@return.cidade,
+                    UF = consulta.@return.uf
+                };
+
+                return Ok(endereco);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
